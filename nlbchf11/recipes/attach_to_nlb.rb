ef71f9989df1af24f29df1caec9a88b7d9f5ebcf
root <root@ip-172-31-6-132.ec2.internal>
2017-09-21 04:57:51 +0000

    first commit

--- nlbchf11/recipes/attach_to_nlb.rb
+++ nlbchf11/recipes/attach_to_nlb.rb
@@ -26,6 +26,6 @@ bash 'NLB_Register' do
   code <<-EOH
     EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id || die \"wget instance-id has failed: $?\"`"
     EC2_AVAIL_ZONE="`wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone || die \"wget availability-zone has failed: $?\"`"
-    EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
-   aws elbv2 register-targets --target-group-arn $target_group_arn --targets Id=$EC2_INSTANCE_ID --region $EC2_REGION
+    aws elbv2 register-targets --target-group-arn $target_group_arn --targets Id=$EC2_INSTANCE_ID --region us-west-1
+  EOH
 end
